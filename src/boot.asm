2851e59f698175b5333c6056956237a70a415a30
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-19 20:18:12 +0430

    Added: Boot Sector

---
+++ src/boot.asm
@@ -0,0 +1,11 @@
+    ;;;Boot sector loaded by BIOS is 512 bytes
+    ;;;The code in the boot sector of the disk is loaded by the BIOS at 0000:7c00
+    ;;;Machine starts in Real Mode     
+
+hang:                           ;;; Infinite loop
+    jmp hang
+    
+    times 510-($-$$) db 0       ;;; Fill with 510 zeros
+    db 0x55
+    db 0xAA
+    
