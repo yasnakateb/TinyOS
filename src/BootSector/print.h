ed2c317396ebc1fc7e73d4fdf83c5e468e9ba42b
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-20 02:17:15 +0430

    Changed: Bios_Print

--- src/BootSector/print.h
+++ src/BootSector/print.h
@@ -1,15 +1,52 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 Bios_Print:
-   lodsb
-   or al, al  
-   jz done    
-   mov ah, 0x0E
-   mov bh, 0
-   int 0x10
-   jmp Bios_Print
+    pusha
+print_char:
+    mov al, [si] 
+    or al, al
+    jz done
+    mov ah, 0x0e
+    int 0x10 
+    add si, 1
+    jmp print_char
 done:
-   ret
+    popa
+    ret
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+Bios_Print_Hex:
+    pusha
+    mov cx, 0
+
+hex_loop:
+    cmp cx, 4 
+    je end
+    mov ax, dx 
+    and ax, 0x000f 
+    add al, 0x30 
+    cmp al, 0x39 
+    jle position
+    add al, 7 
+
+position:
+    
+    mov bx, hex_number + 5 
+    sub bx, cx  
+    mov [bx], al
+    ror dx, 4 
+    add cx, 1
+    jmp hex_loop
+
+end:
+    mov bx, hex_number
+    call Bios_Print
+
+    popa
+    ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+hex_number   db '0x0000',0
