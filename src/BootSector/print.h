6383ded9c040753bfe4a5b958975e6dbb81f512a
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-20 02:25:55 +0430

    Added: new line char

--- src/BootSector/print.h
+++ src/BootSector/print.h
@@ -11,6 +11,11 @@ print_char:
     add si, 1
     jmp print_char
 done:
+    mov ah, 0x0e
+    mov al, 0x0a ; newline char
+    int 0x10
+    mov al, 0x0d ; carriage return
+    int 0x10
     popa
     ret
 
