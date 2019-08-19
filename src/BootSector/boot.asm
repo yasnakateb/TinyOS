bf92cbab5e8389d7cd2a74866a762fb85a1b41fd
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-20 00:56:21 +0430

    Added: print.h

--- src/BootSector/boot.asm
+++ src/BootSector/boot.asm
@@ -3,21 +3,30 @@
     ;;;Machine starts in Real Mode     
 [org 0x7c00]
 
-%include "bios.h"
 
 start:
     xor ax, ax
     mov ds, ax
     cld
  
-    BiosPrint message
-    
+    mov si, message
+    call Bios_Print
+    mov si, msg_read_from_disk
+    call Bios_Print
+
+
+%include 'print.h'
+
 hang:                           ;;; Infinite loop
     jmp hang
 
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     
     message   db 'Have fun, you guys', 13, 10, 0
+    msg_read_from_disk db 'Reading data from a disk in order to run the kernel', 13, 10, 0
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
     times 510-($-$$) db 0       ;;; Fill with 510 zeros
     db 0x55
     db 0xAA
