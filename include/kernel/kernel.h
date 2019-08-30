1660d08e3dd6aaadb73062e35d906d848f736ec8
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-30 21:23:32 +0430

    Added: kernel.h

---
+++ include/kernel/kernel.h
@@ -0,0 +1,35 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+
+#define VGA_ADDRESS 0xB8000
+#define BUFSIZE 20
+
+uint16* vga_buffer;
+
+#define NULL 0
+
+enum vga_color {
+    COLOR_BLACK             = 0,
+    COLOR_BLUE              = 1,
+    COLOR_GREEN             = 2,
+    COLOR_CYAN              = 3,
+    COLOR_RED               = 4,
+    COLOR_MAGENTA           = 5,
+    COLOR_BROWN             = 6,
+    COLOR_LIGHT_GREY        = 7,
+    COLOR_DARK_GREY         = 8,
+    COLOR_LIGHT_BLUE        = 9,
+    COLOR_LIGHT_GREEN       = 10,
+    COLOR_LIGHT_CYAN        = 11,
+    COLOR_LIGHT_RED         = 12,
+    COLOR_LIGHT_MAGENTA     = 13,
+    COLOR_LIGHT_BROWN       = 14,
+    COLOR_WHITE             = 15,
+};
+
+#endif
