8aeb666c715062bb0cb37abbed8e36c4643f15bf
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-30 16:36:11 +0430

    Added: kernel folder and kernel.c

---
+++ src/kernel/kernel.c
@@ -0,0 +1,8 @@
+
+void main() 
+{
+    char* character = (char*) 0xb8000;              ///  Create a pointer  to a char , and  point it to the  first  text  cell of
+                                                    ///  video  memory
+    *character = 'X';       
+
+}
