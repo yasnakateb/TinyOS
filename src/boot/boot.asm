f2d9823fa5cf6152ef31b1e6febd875aaa7c656f
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-30 17:05:35 +0430

    Added: Load_Kernel

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,9 +1,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; BOOT SECTOR
 ;;; Boot sector loaded by BIOS is 512 bytes
-;;; The code in the boot sector of the disk is loaded by the BIOS at 0000:7c00
-;;; Machine starts in Real Mode. Then it switches from 16-bit real mode 
-;;; into 32-bit protected mode.
+;;; The code in the boot sector of the disk is loaded by the BIOS at 
+;;; 0000:7c00 Machine starts in Real Mode. Then it switches from 16-bit 
+;;; real mode into 32-bit protected mode.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                                 
 [org 0x7c00]
@@ -17,6 +17,8 @@ Start:
     mov bx, msg_Real_Mode
     call Real_Mode_Print
 
+    call  Load_Kernel           ;;; Load kernel
+
     call Switch_To_Protected_Mode
     jmp $
 
@@ -26,6 +28,27 @@ Start:
 %include 'include/protected_mode/switch.inc'
 %include 'include/protected_mode/gdt.inc'
 %include 'include/real_mode/disk.inc'
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; 16-bit Real Mode
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                
+[bits  16]
+
+Load_Kernel:
+    mov bx, msg_Load_Kernel     ;;; Load kernel
+    call msg_Load_Kernel
+    mov bx, KERNEL_OFFSET       ;;; Read from disk and store in 0x1000
+    mov dh, 15                  ;;; Load  the  first  15  sectors 
+    mov dl, [BOOT_DRIVE]        ;;; Drive number      
+    call Disk_Load              ;;; INT 0x13
+    ret
+
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -46,7 +69,8 @@ Begin_Protected_Mode:           ;;; After the switch we will get here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     msg_Real_Mode db "Real mode", 0
-    msg_Protected_Mode db "Protected mode", 0   
+    msg_Protected_Mode db "Protected mode", 0 
+    msg_Load_Kernel db "Loading  kernel  into  memory.", 0  
         
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
