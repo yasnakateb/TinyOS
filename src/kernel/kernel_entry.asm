becf4798d9f02a9ef27ba51da36822f73d2bf0d4
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-30 19:41:30 +0430

    Updated: kernel_entry.asm

--- src/kernel/kernel_entry.asm
+++ src/kernel/kernel_entry.asm
@@ -1,5 +1,5 @@
-[bits 32]						;;; In  protected  mode 
-														
-[extern main] 					;;; Referencing  the  external  symbol  ’main ’
-call main 						;;; Invoke  main() in our C kernel
+[bits 32]                       ;;; In  protected  mode 
+                                                        
+[extern main]                   ;;; Referencing  the  external  symbol  ’main ’
+call main                       ;;; Invoke  main() in our C kernel
 jmp $
