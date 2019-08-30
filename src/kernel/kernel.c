2f529d9a06fd7e2ba1b617899c502f11db76f4ae
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-30 16:44:25 +0430

    Added: comment

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,6 +3,6 @@ void main()
 {
     char* character = (char*) 0xb8000;              ///  Create a pointer  to a char , and  point it to the  first  text  cell of
                                                     ///  video  memory
-    *character = 'X';       
+    *character = 'X';                               ///  Store  the  character  ’X’
 
 }
